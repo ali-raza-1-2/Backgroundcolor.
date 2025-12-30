bbc9e7feb3d63617bfb751a83b6665eafdd91ae5
aliraza <alirazafarhan12@gmail.com>
2025-12-30 20:47:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var changeColor = document.getElementById("col");
+
+function colorcyan() {
+        changeColor.style.backgroundColor = 'cyan'
+}
+
+function colormagenta() {
+        changeColor.style.backgroundColor = 'Magenta'
+}
+
+function coloryellow() {
+        changeColor.style.backgroundColor = 'Yellow'
+}
+
+function colorblack() {
+        changeColor.style.backgroundColor = 'Black' 
+}
+
+function clearColor() {
+        changeColor.removeAttribute('style');
+}
